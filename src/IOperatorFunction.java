ca7009ab7669294720772c082728b5daa50a3e27
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-03-29 22:56:58 +0700

    first commit

---
+++ src/IOperatorFunction.java
@@ -0,0 +1,14 @@
+interface IOperatorFunction {
+	void createBankAccount(String idCard, String username, String phone, String address, String email);
+
+	void lockBankAccount(String bankAccountID);
+
+	void viewCusList();
+
+	void updateCusList(String idCard, String username, String phone, String address, String email);
+
+	void withdraw(String bankAccountID, Double withdrawMoney);
+
+	void deposit(String bankAccountID, Double depositMoney);
+
+}
