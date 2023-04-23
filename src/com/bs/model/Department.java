568c898e2ed6be5776e153de984fcb179b1565eb
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-04-23 22:16:21 +0700

    model department

--- src/com/bs/model/Department.java
+++ src/com/bs/model/Department.java
@@ -2,23 +2,28 @@ package com.bs.model;
 
 public class Department {
 	Integer id;
-	String name;
-	public Department(Integer id, String name) {
-	
+	String department_name;
+
+	public Department(Integer id, String department_name) {
+
 		this.id = id;
-		this.name = name;
+		this.department_name = department_name;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
-		return name;
+		return department_name;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setName(String department_name) {
+		this.department_name = department_name;
 	}
-	
+
 }
