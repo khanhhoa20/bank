44652c4401a1f78b565f44526e684bd88a2ac2b1
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-04-23 20:23:40 +0700

    sth

--- src/com/bs/dao/UserDAO.java
+++ src/com/bs/dao/UserDAO.java
@@ -1,5 +1,5 @@
 package com.bs.dao;
-
+//
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,6 +18,28 @@ public class UserDAO {
 	}
 
 	public String login(String username, String password) {
+		String role = null;
+		try {
+			if (connection == null || connection.isClosed()) {
+				connection = DBUtil.getConnection();
+			}
+			preparedStatement = connection
+					.prepareStatement(IUserQuery.SELECT_QUERY);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, password);
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				role = resultSet.getString(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.closeConnection();
+		}
+		return role;
+	}
+	public String addUser(String username, String password) {
 		String role = null;
 		try {
 			if (connection == null || connection.isClosed()) {
