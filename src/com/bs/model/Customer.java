25b0aa8efaf9368aed3506ba50f1c4b1fc006bf7
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-04-24 13:35:42 +0700

    some change

--- src/com/bs/model/Customer.java
+++ src/com/bs/model/Customer.java
@@ -11,61 +11,77 @@ public class Customer extends User {
 	private long nationalId;
 	private Date dateOfBirth;
 	private User user;
-	
+
 	public long getCustomerId() {
 		return customerId;
 	}
+
 	public void setId(long customerId) {
 		this.customerId = customerId;
 	}
+
 	public String getCustomerName() {
 		return customerName;
 	}
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public long getNationalId() {
 		return nationalId;
 	}
+
 	public void setNationalId(long nationalId) {
 		this.nationalId = nationalId;
 	}
+
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
+
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	public Customer() {}
-	
-	public Customer(long customerId, String customerName, String address, String phoneNumber, String email, long nationalId, Date dateOfBirth, User user) {
-		super(user.getId(), user.getUsername(), user.getPass(), user.getRole());
+
+	public Customer() {
+	}
+
+	public Customer(long customerId, String customerName, String address, String phoneNumber, String email,
+			long nationalId, Date dateOfBirth, User user) {
+
 		this.customerId = customerId;
 		this.customerName = customerName;
 		this.address = address;
@@ -75,9 +91,10 @@ public class Customer extends User {
 		this.dateOfBirth = dateOfBirth;
 		this.user = user;
 	}
-	
-	public Customer(long customerId, String customerName, String address, String phoneNumber, String email, long nationalId, Date dateOfBirth) {
-		super();
+
+	public Customer(long customerId, String customerName, String address, String phoneNumber, String email,
+			long nationalId, Date dateOfBirth) {
+
 		this.customerId = customerId;
 		this.customerName = customerName;
 		this.address = address;
@@ -86,5 +103,5 @@ public class Customer extends User {
 		this.nationalId = nationalId;
 		this.dateOfBirth = dateOfBirth;
 	}
-	
+
 }
