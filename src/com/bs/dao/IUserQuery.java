0cd665b17793f1468a3c331232c0e7b780af57fc
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-04-24 16:19:07 +0700

    sth

--- src/com/bs/dao/IUserQuery.java
+++ src/com/bs/dao/IUserQuery.java
@@ -1,5 +1,9 @@
 package com.bs.dao;
 
+/**
+ * @author khánh hòa
+ *
+ */
 public interface IUserQuery {
 	String SELECT_QUERY = "SELECT role FROM \"user\" WHERE user_name = ? and user_pass = ?";
 	String INSERT_QUERY = "INSERT INTO \"user\" VALUES (null,?,?,?)";
