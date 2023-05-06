c5934098734f4409221b6a69a734e151c9a08e31
phucnt2002 <nguyenphuc2002ok@gmail.com>
2023-05-06 20:44:57 +0700

    schedulePlanDepart and DAO

---
+++ src/com/bs/dao/SchedulePlanDepartmentDAO.java
@@ -0,0 +1,107 @@
+package com.bs.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.bs.model.SchedulePlanDepartment;
+import com.bs.util.DBUtil;
+import java.util.*;
+
+public class SchedulePlanDepartmentDAO {
+	public boolean insert(SchedulePlanDepartment spd) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		boolean result = false;
+		try {
+			stmt = conn.prepareStatement(
+					"INSERT INTO schedule_plan_department (department_id, schedule_plan_id) VALUES (?, ?)");
+			stmt.setLong(1, spd.getDepartmentID());
+			stmt.setLong(2, spd.getSchedulePlanID());
+			int rowsAffected = stmt.executeUpdate();
+			if (rowsAffected > 0) {
+				result = true;
+			}
+		} finally {
+			DBUtil.closeConnection();
+		}
+		return result;
+	}
+
+	public List<SchedulePlanDepartment> find(long departmentID, long schedulePlanID) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		List<SchedulePlanDepartment> list = new ArrayList<SchedulePlanDepartment>();
+		try {
+			stmt = conn.prepareStatement(
+					"SELECT * FROM schedule_plan_department WHERE department_id = ? AND schedule_plan_id = ?");
+			stmt.setLong(1, departmentID);
+			stmt.setLong(2, schedulePlanID);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				SchedulePlanDepartment spd = new SchedulePlanDepartment();
+				spd.setDepartmentID(rs.getLong("department_id"));
+				spd.setSchedulePlanID(rs.getLong("schedule_plan_id"));
+				list.add(spd);
+			}
+		} finally {
+			DBUtil.closeConnection();
+		}
+		return list;
+	}
+
+	public List<SchedulePlanDepartment> findByDepartmentID(long departmentID) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		List<SchedulePlanDepartment> list = new ArrayList<SchedulePlanDepartment>();
+		try {
+			stmt = conn.prepareStatement("SELECT * FROM schedule_plan_department WHERE department_id = ?");
+			stmt.setLong(1, departmentID);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				SchedulePlanDepartment spd = new SchedulePlanDepartment();
+				spd.setDepartmentID(rs.getLong("department_id"));
+				spd.setSchedulePlanID(rs.getLong("schedule_plan_id"));
+//	                spd.setDepartmentID(rs.getLong("department_id"));
+				list.add(spd);
+			}
+		} finally {
+			DBUtil.closeConnection();
+		}
+		return list;
+	}
+
+	public int delete(SchedulePlanDepartment spd) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		int rowsDeleted = 0;
+		try {
+			stmt = conn.prepareStatement(
+					"DELETE FROM schedule_plan_department WHERE department_id = ? AND schedule_plan_id = ?");
+			stmt.setLong(1, spd.getDepartmentID());
+			stmt.setLong(2, spd.getSchedulePlanID());
+			rowsDeleted = stmt.executeUpdate();
+		} finally {
+			DBUtil.closeConnection();
+		}
+		return rowsDeleted;
+	}
+
+	public int update(SchedulePlanDepartment spd) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement stmt = null;
+		int rowsUpdated = 0;
+		try {
+			stmt = conn.prepareStatement(
+					"UPDATE schedule_plan_department SET schedule_plan_id = ? WHERE department_id = ?");
+			stmt.setLong(1, spd.getSchedulePlanID());
+			stmt.setLong(2, spd.getDepartmentID());
+			rowsUpdated = stmt.executeUpdate();
+		} finally {
+			DBUtil.closeConnection();
+		}
+		return rowsUpdated;
+	}
+}
---
+++ src/com/bs/model/SchedulePlanDepartment.java
@@ -0,0 +1,18 @@
+package com.bs.model;
+
+public class SchedulePlanDepartment {
+	private long departmentID;
+	private long schedulePlanID;
+	public long getDepartmentID() {
+		return departmentID;
+	}
+	public void setDepartmentID(long departmentID) {
+		this.departmentID = departmentID;
+	}
+	public long getSchedulePlanID() {
+		return schedulePlanID;
+	}
+	public void setSchedulePlanID(long schedulePlanID) {
+		this.schedulePlanID = schedulePlanID;
+	}
+}
